f9c90a94351ac76e7ee0dd39f92321e724b7087e
Abhi <abhidashing.singh@gmail.com>
2019-03-03 01:10:31 +0530

    keras

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask
 from datetime import datetime
+import pandas as pd
+import tensorflow as tf
+import keras
+from keras.models import load_model
+
 app = Flask(__name__)
+
+def auc(y_true, y_pred):
+    auc = tf.metrics.auc(y_true, y_pred)[1]
+   keras.backend.get_session().run(tf.local_variables_initializer())
+    return auc
+
+global graph
+graph = tf.get_default_graph()
+model = load_model('classifier.h5', custom_objects={'auc': auc})
+
+
 
 @app.route('/')
 def homepage():
@@ -11,6 +27,25 @@ def homepage():
     <p>It is currently {time}.</p>
     <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
+
+
+@app.route("/predict", methods=["GET","POST"])
+def predict():
+    data = {"success": False}
+
+    params = flask.request.json
+    if (params == None):
+        params = flask.request.args
+
+    # if parameters are found, return a prediction
+    if (params != None):
+        x=pd.DataFrame.from_dict(params, orient='index').transpose()
+        with graph.as_default():
+            data["prediction"] = str(model.predict(x)[0][0])
+            data["success"] = True
+
+    # return a response in json format 
+    return flask.jsonify(data)  
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
