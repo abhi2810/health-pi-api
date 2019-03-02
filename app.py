b6c514691a533550d22e18e70295a212212927e1
Abhi <abhidashing.singh@gmail.com>
2019-03-03 02:48:44 +0530

    fixed indentation error

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def predict():
     parameters.append(flask.request.args.get('temp'))
     parameters.append(flask.request.args.get('hr'))
     parameters.append(flask.request.args.get('bmi'))
-    if len(parameters) == 3 :
+    if parameters[0] == "" :
         inputFeature = np.asarray(parameters).reshape(1, 3)
         with graph.as_default():
             raw_prediction = model.predict(inputFeature)[0][0]
